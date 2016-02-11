d8bf111a6e595e1630e915e4c95168f5a6f02570
poine <poinix@gmail.com>
2016-02-11 06:39:05 +0100

    added binary version of vocabulary to speedup ORB_SLAM2 start

--- build.sh
+++ build.sh
@@ -37,4 +37,8 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j
+cd ..
+
+echo "Converting vocabulary to binary"
+./tools/bin_vocabulary
